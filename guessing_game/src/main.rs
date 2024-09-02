6379b2024b4282cfc609fa9a7b2a1d81d0151c35
jonasvsc <jonasvasconcelosbarbosa@gmail.com>
2024-09-02 01:14:58 -0300

    Chapter 1 & 2

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(0..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new(); // mutabble
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
+
+
+}
